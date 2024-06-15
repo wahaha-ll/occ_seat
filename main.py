19e41a3ded625d670fc56d79977315d7029d825d
LLVE <69597618+wahaha-ll@users.noreply.github.com>
2024-06-15 14:55:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import requests
-import json
-import time
+
+headers = {
+    'Authorization': 'junyue-server MToxODU2NjpmY2VmM2MzMDM4ZmVkOGM3NjlkOTczMTE5NTM5MTdkMQ==',
+}
+
+json_data = {
+    'deviceMac': '70B3D5058005005E',
+}
+
+response = requests.post('http://202.203.134.159:10009/v1/reservation/devices/socket/off', headers=headers, json=json_data)
