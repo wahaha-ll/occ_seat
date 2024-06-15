256e91076881766ef088ca79c34102bc5011baa1
LLVE <69597618+wahaha-ll@users.noreply.github.com>
2024-06-15 15:57:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,78 @@
 import requests
+import json
+import time
 
-response = requests.get("https://www.bing.com")
+Author : list[dict] = [
+    {"work" : True,
+     "name" : 'junyue-server MToxODU2NjpkMzgzNTE5ZWMwOWQ3ZDdlNzZhYWE4OTAyNTkwYWE3Mw==',
+     "resourceId" : 760,
+     "deviceMac" : "70B3D5058005005E",
+     "id" : 0}
+]
 
-print(response.content.decode())
+headers = {
+    'Authorization': "",
+}
 
-while True:
-  pass
+json_data = {"use":{
+                    'resourceId': 0,
+                    'startTime': 0,
+                    'endTime': 0,
+                    },
+             "device":{
+                    "deviceMac" : ""
+                    }
+}
+
+USE = "use"
+FINISH = lambda id : "finish/" + str(id)
+CURRENT = "current"
+DEVICE = "devices/socket/on"
+WEB_URL = lambda fun : "http://202.203.134.159:10009/v1/reservation/" + fun
+ENDTIME = 0
+
+def get_current():
+    for auth in Author:
+        headers["Authorization"] = auth["name"] 
+        response = requests.get(WEB_URL(CURRENT), headers=headers)
+        if response.content.decode() != '':
+            auth["id"] = json.loads(response.content.decode())["id"]
+            fin_seat()
+
+def occ_seat():
+    global ENDTIME
+    for auth in Author:
+        headers["Authorization"] = auth["name"]
+        json_data["use"]["resourceId"] = auth["resourceId"]
+        json_data["device"]["deviceMac"] = auth["deviceMac"]
+        try:
+            response = requests.post(WEB_URL(USE), headers=headers, json=json_data["use"])
+        except:
+            pass
+        content = json.loads(response.content.decode())
+        try:
+            requests.post(WEB_URL(DEVICE),headers=headers, json=json_data["device"])
+        except:
+            pass
+        auth["id"] = content["id"]
+        ENDTIME = content["endTime"]
+
+def fin_seat():
+    global ENDTIME
+    for auth in Author:
+        headers["Authorization"] = auth["name"]
+        try:
+            requests.post(WEB_URL(FINISH(auth["id"])), headers=headers)
+        except:
+            pass
+        ENDTIME = 0
+
+if "__main__" == __name__:
+    # get_current()
+    while True:
+        if ENDTIME == 0 and 22 >= time.localtime().tm_hour >= 8:
+            occ_seat()
+        elif ENDTIME <= time.time()*1000-1000:
+            fin_seat()
+        elif time.localtime().tm_hour > 22:
+            fin_seat()
